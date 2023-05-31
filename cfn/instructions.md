63bc6fdb3d229b20ddd5cc9705b665a42270847b
JabrilMahamud <Jabril.mahamud@bp.com>
2023-05-31 10:43:14 +0100

    late update

---
+++ cfn/instructions.md
@@ -0,0 +1,5 @@
+For each instance create a set of tags:
+    - "Name": A unique name for the instance not already present in the VPC
+    - "bp-unique-name": A unique name for the instance not already present in the VPC
+    - "cip-compliance": 
+    - "bp-unique-domain-ou": 
--- platform/lambda.py
+++ platform/lambda.py
@@ -1,16 +1,40 @@
-def lambda_handler(event, context):
-    import boto3
-    sqs = boto3.resources('sqs')
-    queue = sqs.get_queue_by_name(QueueName='my-queue')
-    response = queue.send_message(
-        MessageBody="boto3",
-        MessageAttributes={
-            'KeyPair': {
-                'Key1': 'Pair1',
-                'Key2': 'Pair2'
-            }
-        }
-    )
+# def lambda_handler(event, context):
+import json
 
-    return response.get('MessageId')
-    return response.get('MD5OfMessageBody')
+def get_keypair_from_json(json_file, item):
+    with open(json_file) as file:
+        data = json.load(file)
+        return get_keypair_recursive(data, item)
+
+def get_keypair_recursive(data, item):
+    results = []  # Store matching key-value pairs
+
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == item:
+                results.append((key, value))
+            if isinstance(value, (dict, list)):
+                results.extend(get_keypair_recursive(value, item))
+    elif isinstance(data, list):
+        for item_value in data:
+            results.extend(get_keypair_recursive(item_value, item))
+
+    return results
+
+# Example usage
+json_file_path = "C:/Users/maham/OneDrive/Desktop/cfn (1)/cfn/ct_event.json"
+item_to_find = "instancesSet"
+
+# Find key-value pairs in JSON
+results = get_keypair_from_json(json_file_path, item_to_find)
+
+# Print the results
+if results:
+    print(f"Found {len(results)} occurrences of '{item_to_find}' in the JSON:")
+    for i, (key, value) in enumerate(results, start=1):
+        print(f"\nMatch {i}:")
+        print("Key:", key)
+        print("Value:")
+        print(json.dumps(value, indent=4))
+else:
+    print("Item not found in the JSON.")
