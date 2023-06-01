22c06e8777a24f95ba4c83f800f372408925a2e1
JabrilMahamud <Jabril.mahamud@bp.com>
2023-06-01 18:42:15 +0100

    Success?

--- platform/lambda.py
+++ platform/lambda.py
@@ -1,66 +1,48 @@
-# def lambda_handler(event, context):
+import boto3
 import json
 
-def get_keypair_from_json(json_file, item):
-    with open(json_file) as file:
-        data = json.load(file)
-        return get_keypair_recursive(data, item)
-
-def get_keypair_recursive(data, item):
-    results = []  # Store matching key-value pairs
-
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if key == item:
-                results.append((key, value))
-            if isinstance(value, (dict, list)):
-                results.extend(get_keypair_recursive(value, item))
-    elif isinstance(data, list):
-        for item_value in data:
-            results.extend(get_keypair_recursive(item_value, item))
-    return results
-
-def create_keypair_dictionary(results):
-    keypair_dict = {}
-    for key, value in results:
-        if key in keypair_dict:
-            keypair_dict[key].append(value)
-        else:
-            keypair_dict[key] = [value]
-    return keypair_dict
-
-# Example usage
-json_file_path = "C:/Users/maham/OneDrive/Desktop/cfn (1)/cfn/ct_event.json"
-item_to_find = "tags"
-
-# Find key-value pairs in JSON
-results = get_keypair_from_json(json_file_path, item_to_find)
-
-# Create dictionary from results
-keypair_dict = create_keypair_dictionary(results)
-
-# Print the dictionary
-if keypair_dict:
-    print("Key-Value Dictionary:")
-    print(json.dumps(keypair_dict, indent=4))
-else:
-    print("Item not found in the JSON.")
+# Function to tag an EC2 instance
+def tag_ec2_instance(instance_id, tags):
+    # Create a Boto3 EC2 client
+    ec2_client = boto3.client('ec2', region_name='eu-west-2')
+    
+    # Convert the hash table into a list of dictionaries
+    tag_list = [{'Key': key, 'Value': value} for key, value in tags.items()]
+    
+    # Tag the EC2 instance
+    response = ec2_client.create_tags(Resources=[instance_id], Tags=tag_list)
+    
+    # Check if the tagging was successful
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        print('Instance tagged successfully.')
+    else:
+        print('Failed to tag instance:', response)
 
 
-# # Example usage
-# json_file_path = "C:/Users/maham/OneDrive/Desktop/cfn (1)/cfn/ct_event.json"
-# item_to_find = "tags"
+# Read the JSON file
+with open(r'C:/Users/maham/OneDrive/Desktop/github repo/github repo/Synoptic/cfn/ct_event.json') as file:
+    data = json.load(file)
 
-# # Find key-value pairs in JSON
-# results = get_keypair_from_json(json_file_path, item_to_find)
+# Extract the tags from the JSON data
+tags = data['event']['detail']['requestParameters']['tagSpecificationSet']['items']
 
-# # Print the results
-# if results:
-#     print(f"Found {len(results)} occurrences of '{item_to_find}' in the JSON:")
-#     for i, (key, value) in enumerate(results, start=1):
-#         print(f"\nMatch {i}:")
-#         print("Key:", key)
-#         print("Value:")
-#         print(json.dumps(value, indent=4))
-# else:
-#     print("Item not found in the JSON.")
+# Create a hash table to store the key-value pairs
+hash_table = {}
+
+# Populate the hash table with the key-value pairs
+for tag in tags:
+    for item in tag['tags']:
+        key = item['key']
+        value = item['value']
+        hash_table[key] = value
+
+# Print the hash table
+for key, value in hash_table.items():
+    print(f"Key: {key}, Value: {value}")
+
+# Select an EC2 instance ID (replace with your own logic to choose the instance)
+instance_id = 'i-0f634885c5daddc11'
+
+# Tag the selected EC2 instance with the key-value pairs
+tag_ec2_instance(instance_id, hash_table)
+print("success?")
